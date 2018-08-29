4d0ff4e7019314a32e6df0346d34e78d49a7b30d
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-08-29 23:58:27 +0200

    angular

---
+++ angular/angular_notes.sh
@@ -0,0 +1,9 @@
+sudo apt-get install nodejs
+node --version
+sudo apt-get install npm
+sudo npm install -g typescript
+tsc --version
+# download bootstrap
+unzip bootstrap-3.3.7-dist.zip -d ~/projects/AngularTutorial/
+sudo npm install -g @angular/cli
+ng
