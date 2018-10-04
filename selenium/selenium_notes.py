810ea9eae99581e6c4904b284ffe63483396b414
stanojlovic.marko@gmail.com <stanojlovic.marko@gmail.com>
2018-10-04 11:41:05 +0200

    sel upd

--- selenium/selenium_notes.py
+++ selenium/selenium_notes.py
@@ -2,3 +2,19 @@
 
 # JS code to wait until page load is complete
 drv.execute_script("return document.readyState") # returns 'complete'
+
+# get html code of an element
+elem.get_attribute('outerHTML')
+# get html code inside the element
+elem.get_attribute('innerHTML')
+# get element text
+elem.text
+
+# get the list of all attributes of one element
+element = driver.find_element_by_css_selector("img")
+driver.execute_script(\
+'var items = {};\
+for (index = 0; index < arguments[0].attributes.length; ++index)\
+{ items[arguments[0].attributes[index].name] = arguments[0].attributes[index].value };\
+return items;', element)
+{'class': 'frame', 'src': 'yoda.jpg'} # RESULT
