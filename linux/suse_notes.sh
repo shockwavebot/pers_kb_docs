2ab97d3ef6b3b422fe46813ff49676af6259ef69
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-10-12 16:27:40 +0200

    mouse poiter size linux

---
+++ linux/suse_notes.sh
@@ -0,0 +1,2 @@
+# change the mouse pointer size 
+gsettings set org.gnome.desktop.interface cursor-size 40
---
+++ linux/ubuntu18_notes.sh
@@ -0,0 +1,2 @@
+# change the mouse pointer size
+gsettings set com.ubuntu.user-interface.desktop cursor-size 40
