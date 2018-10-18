90768a4b31b67e7a32488fb4cfa60c23134d3b47
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-10-18 16:54:46 +0200

    dhcp host name setup disabled

--- bash/network_notes.sh
+++ bash/network_notes.sh
@@ -4,3 +4,6 @@ telenet 10.216.73.237 1600
 
 # add route
 sudo ip route add 44.81.0.0/18 via 44.81.0.1 dev eth0
+
+# disable dhcp to set change host name 
+sed -i '/^DHCLIENT_SET_HOSTNAME/c\DHCLIENT_SET_HOSTNAME="no"' /etc/sysconfig/network/dhcp
--- linux/common_linux_notes.sh
+++ linux/common_linux_notes.sh
@@ -1,2 +1,8 @@
 # how to check system memory, how much RAM memory is on the server?
 cat /proc/meminfo |grep MemTotal
+
+# not asking for ssh adding new host
+sudo sed -i "/StrictHostKeyChecking/c\StrictHostKeyChecking no" /etc/ssh/ssh_config
+
+# disable dhcp to set change host name
+sed -i '/^DHCLIENT_SET_HOSTNAME/c\DHCLIENT_SET_HOSTNAME="no"' /etc/sysconfig/network/dhcp
