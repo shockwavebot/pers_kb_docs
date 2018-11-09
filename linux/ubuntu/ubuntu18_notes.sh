0e2009e62576689c38b2a662a0a74f515f810ed3
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-11-09 20:25:53 +0100

    ubuntu notes

--- linux/ubuntu/ubuntu18_notes.sh
+++ linux/ubuntu/ubuntu18_notes.sh
@@ -1,2 +1,14 @@
 # change the mouse pointer size
 gsettings set com.ubuntu.user-interface.desktop cursor-size 40
+
+# install chrome browser
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+
+# passwordless sudo
+sudo sed -i '/^\%sudo/c\\%sudo ALL=\(ALL\:ALL\) NOPASSWD\: ALL' /etc/sudoers
+
+# location of background wallpapers
+/usr/share/backgrounds/
