dc977a765955452170237d68fac5a5a84e784b33
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-11-10 13:37:56 +0100

    docker commands

---
+++ docker/docker_commands.sh
@@ -0,0 +1,9 @@
+# Install Docker
+sudo zypper in -y docker # SUSE
+sudo apt-get install docker.io # ubuntu 18
+
+# List downloaded images
+sudo docker images
+
+# List containers (running)
+sudo docker ps 
