d66ec0f0ff2cf8f5d6e34c0911b165edf22632bf
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-11-12 17:02:46 +0100

    docker rm image and cont

--- docker/docker_commands.sh
+++ docker/docker_commands.sh
@@ -5,5 +5,11 @@ sudo apt-get install docker.io # ubuntu 18
 # List downloaded images
 sudo docker images
 
-# List containers (running)
-sudo docker ps 
+# List containers (running/all)
+sudo docker ps -a
+
+# Delete container
+sudo docker rm _id_
+
+# Delete images
+sudo docker image rm _name_
