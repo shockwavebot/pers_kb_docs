465821b4d596c257c0e86c7f848056920bb422bf
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-11-27 14:48:13 +0100

    python: string methods

--- python/python_notes.py
+++ python/python_notes.py
@@ -1,6 +1,54 @@
 
 
 #############################################################
+# string manipulation
+# string methods https://docs.python.org/3/library/stdtypes.html#string-methods
+import string
+'rema car'.replace('car','kralj')
+str_to_dict = set('car')
+str_to_dict.issubset('rema car')
+
+capitalize()            Converts the first character to upper case
+casefold()              Converts string into lower case
+center()                Returns a centered string
+count()	                Returns the number of times a specified value occurs in a string
+encode()                Returns an encoded version of the string
+endswith()              Returns true if the string ends with the specified value
+expandtabs()            Sets the tab size of the string
+find();index()          Searches the string for a specified value and returns the position of where it was found
+format()	            Formats specified values in a string
+isalnum()               Returns True if all characters in the string are alphanumeric
+isalpha()               Returns True if all characters in the string are in the alphabet
+isdecimal()             Returns True if all characters in the string are decimals
+isdigit()               Returns True if all characters in the string are digits
+isidentifier()          Returns True if the string is an identifier
+islower()               Returns True if all characters in the string are lower case
+isnumeric()             Returns True if all characters in the string are numeric
+isprintable()           Returns True if all characters in the string are printable
+isspace()               Returns True if all characters in the string are whitespaces
+istitle()               Returns True if the string follows the rules of a title
+isupper()               Returns True if all characters in the string are upper case
+join()                  Joins the elements of an iterable to the end of the string
+ljust()                 Returns a left justified version of the string
+lstrip()                Returns a left trim version of the string
+maketrans()             Returns a translation table to be used in translations.
+partition()             Returns a tuple where the string is parted into three parts
+replace()               Returns a string where a specified value is replaced with a specified value
+rfind()                 Searches the string for a specified value and returns the last position of where it was found
+rindex()                Searches the string for a specified value and returns the last position of where it was found
+rpartition()            Returns a tuple where the string is parted into three parts
+rsplit()                Splits the string at the specified separator, and returns a list
+rstrip()                Returns a right trim version of the string
+split()                 Splits the string at the specified separator, and returns a list
+splitlines()            Splits the string at line breaks and returns a list
+startswith()            Returns true if the string starts with the specified value
+swapcase()              Swaps cases, lower case becomes upper case and vice versa
+title()                 Converts the first character of each word to upper case
+translate()             Returns a translated string
+zfill()                 Fills the string with a specified number of 0 values at the beginning
+#############################################################
+
+#############################################################
 # binary operations
 >>> bin(9)
 '0b1001'
@@ -226,8 +274,6 @@ response = {
 return response.get(phraseType, 'Whatever.')
 
 #####################################################
-
-
 
 # random string of N lenght
 import string
