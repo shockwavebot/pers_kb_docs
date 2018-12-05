dfdd295e6192eeac7ae205fefd0d169b1587f70c
Marko Stanojlovic <stanojlovic.marko@gmail.com>
2018-12-05 16:33:20 +0100

    ceph rbd upd

--- ceph/rbd_notes.sh
+++ ceph/rbd_notes.sh
@@ -3,9 +3,9 @@
 # persistent rbd image
 POOL=rbd_pers
 RBD_IMG=tst_rbd_img
-
 # @admin:
 ceph osd pool create $POOL 32 32
+ceph osd pool application enable $POOL rbd
 rbd -p $POOL ls|grep $RBD_IMG && rbd -p $POOL rm $RBD_IMG
 rbd -p $POOL create $RBD_IMG --size 1G
 # @client:
@@ -14,8 +14,10 @@ rbd map ${POOL}/${RBD_IMG}
 RBD_DEV=$(rbd showmapped|grep $RBD_IMG|awk '{print $5}')
 sgdisk --largest-new=1 $RBD_DEV
 mkfs.xfs ${RBD_DEV}p1 -f
+rbd unmap $RBD_DEV
 echo "${POOL}/${RBD_IMG} id=admin,keyring=/etc/ceph/ceph.client.admin.keyring"|tee -a /etc/ceph/rbdmap
 systemctl enable rbdmap.service
-# reboot client node 
+systemctl start  rbdmap.service
+systemctl status rbdmap.service
 rbd showmapped|grep $RBD_IMG
 #################################################
