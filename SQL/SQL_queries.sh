2cef72a66aa77aedac45e9a2131b46f5de02f1aa
mstan <stanojlovic.marko@gmail.com>
2019-01-16 11:22:50 +0100

    Create SQL_queries.sh

---
+++ SQL/SQL_queries.sh
@@ -0,0 +1,10 @@
+# filter your columns
+ SELECT col1, col2, col3, ... FROM table1
+# filter the rows
+ WHERE col4 = 1 AND col5 = 2
+# aggregate the data
+ GROUP by …
+# limit aggregated data
+ HAVING count(*) > 1
+# order of the results
+ ORDER BY col2
