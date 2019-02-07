48806e45194d7a4ce33595c04286c107d453bb0a
mstan <stanojlovic.marko@gmail.com>
2019-02-07 11:35:26 +0100

    Update SQL_queries.sh

--- SQL/SQL_queries.sh
+++ SQL/SQL_queries.sh
@@ -8,3 +8,8 @@
  HAVING count(*) > 1
 # order of the results
  ORDER BY col2
+
+# DELETE AN ENTRY 
+DELETE FROM table_name WHERE [condition];
+SELECT * FROM tt_tab_test WHERE vistor_id <= 4; # to verify what will be deleted, if condition is OK 
+DELETE FROM tt_tab_test WHERE vistor_id <= 4;
