9c7f3fcdc20d4df473987d07289f6864539c68ba
mstan <stanojlovic.marko@gmail.com>
2019-02-26 10:58:49 +0100

    Update SELECT_queries.sql

--- postgreSQL/SELECT_queries.sql
+++ postgreSQL/SELECT_queries.sql
@@ -1,3 +1,9 @@
 # last 5 minutes 
 select id, creation_time from ct_table where creation_time >= now() - interval '5 minutes';
 
+# looking for a table name 
+select tablename from pg_catalog.pg_tables where schemaname='public' and tablename like '%ins%';
+select tablename from pg_catalog.pg_tables where tableowner='r_fxs09' and tablename like '%ins%';
+
+
+
