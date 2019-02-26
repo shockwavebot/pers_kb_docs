612d3cf04d6adc58b027930da98ccf339e7f3cc1
mstan <stanojlovic.marko@gmail.com>
2019-02-26 13:25:09 +0100

    Update and rename INSERT.sql to INSERT_UPDATE.sql

--- postgreSQL/INSERT_UPDATE.sql
+++ postgreSQL/INSERT_UPDATE.sql
@@ -4,5 +4,6 @@ insert into table (field_1, field_2, field_3, date_time) values ('dummy1', 'dumm
 # date time insert
 current_timestamp at time zone 'Europe/Berlin'
 
-
+# simple update example 
+update my_table set colomn_value='YES' where colomn_key='NEW_VALUE';
 
