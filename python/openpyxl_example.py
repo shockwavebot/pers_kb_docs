91595ea3367c28efbe4b7e4d2869125af5da91fa
mstan <stanojlovic.marko@gmail.com>
2019-03-07 17:20:59 +0100

    Update openpyxl_example.py

--- python/openpyxl_example.py
+++ python/openpyxl_example.py
@@ -6,3 +6,11 @@ wb = load_workbook(filename='spreadsheet_file.xlsx')
 wb.sheetnames            # to get the tab or sheet names 
 ws = wb['tab_name']
 # search for a value in a cell 
+
+def search(ws, val, row=None, col=None):
+    for col in ws.iter_cols():
+        for cell in col:
+            if cell.value == val:
+                return (cell.row, cell.column)
+                  
+             
