da5a628590def8557e9a1726baac9cf2d21096b5
mstan <stanojlovic.marko@gmail.com>
2019-03-18 12:31:47 +0100

    Update strings.sh

--- bash/strings.sh
+++ bash/strings.sh
@@ -1,3 +1,11 @@
+${#string}           # String Length
+${string#substring}  # Deletes shortest match of $substring from front of $string
+${string##substring} # Deletes longest match of $substring from front of $string
+${string%substring}  # Deletes shortest match of $substring from back of $string
+${string%%substring} # Deletes longest match of $substring from back of $string
+${string/substring/replacement}  # replace
+${string//substring/replacement} # replace all
+
 # Cut parts of the variable/string 
 [me@myhost ~]$ myvar=1234567890
 [me@myhost ~]$ echo ${myvar: 2} # cut from left
