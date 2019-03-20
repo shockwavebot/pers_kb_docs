dd55ce7b57cfbac4c3f30bfc18071d595afea6b9
mstan <stanojlovic.marko@gmail.com>
2019-03-20 17:11:54 +0100

    Update OpenShift_notes.sh

--- OpenShift/OpenShift_notes.sh
+++ OpenShift/OpenShift_notes.sh
@@ -5,3 +5,10 @@ oc types
 oc login https://IP -u <USERNAME>
 oc login https://IP:443 --token=<TOKEN>
 
+# Projects 
+oc projects         # list projects 
+oc project myproj   # switch to myproj
+
+oc get all 
+
+
