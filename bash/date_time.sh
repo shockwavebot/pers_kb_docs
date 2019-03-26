15bbb1a1350935b84cc6eccee06273ae31e48e03
mstan <stanojlovic.marko@gmail.com>
2019-03-26 10:10:30 +0100

    Create date_time.sh

---
+++ bash/date_time.sh
@@ -0,0 +1,10 @@
+# get date 5 minutes before or after 
+date --date='5 minutes ago' # 5min ago
+date # how 
+date --date='5 minutes' # in 5min in from now
+
+# Date format
+date +%Y-%m-%d' '%H:%M:%S
+
+# Calculate date in the future 
+date -d "2017-03-05 +90 days" '+%Y-%m-%d'
