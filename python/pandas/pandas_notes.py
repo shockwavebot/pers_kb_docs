0881641178c417188e47b05da2dee85529944a39
mstan <stanojlovic.marko@gmail.com>
2019-03-27 14:24:34 +0100

    Create pandas_notes.py

---
+++ python/pandas/pandas_notes.py
@@ -0,0 +1,17 @@
+# install: pip3 install pandas
+
+import pandas as pd 
+
+# creating data frame 
+>>> df = pd.DataFrame([[1,2,3],[4,5,6],[7,8,9]], columns=["Col_1","Col_2","Col_3"])
+>>> df
+   Col_1  Col_2  Col_3
+0      1      2      3
+1      4      5      6
+2      7      8      9
+
+>>> df = pd.DataFrame([{"Name":"Yoda", "Age": 735},{"Name":"Luke"}])
+>>> df
+     Age  Name
+0  735.0  Yoda
+1    NaN  Luke
