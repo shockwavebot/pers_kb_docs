1a7db8ad70a7223740c6ac78c6065dc06a5d0055
mstan <stanojlovic.marko@gmail.com>
2019-04-05 12:03:11 +0200

    Update SELECT_queries.sql

--- postgreSQL/SELECT_queries.sql
+++ postgreSQL/SELECT_queries.sql
@@ -6,5 +6,6 @@ select something where my_timestamp < to_timestamp('25-03-2019 14:30:02', 'dd-mm
 select tablename from pg_catalog.pg_tables where schemaname='public' and tablename like '%ins%';
 select tablename from pg_catalog.pg_tables where tableowner='r_fxs09' and tablename like '%ins%';
 
-
+# case insensitive query 
+select cloumn_name from table_name where lower(column_name) like '%filter%'
 
