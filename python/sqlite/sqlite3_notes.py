a819035768ae565e029bcc1d218614e1a01e247a
mstan <stanojlovic.marko@gmail.com>
2019-04-09 11:03:00 +0200

    Create sqlite3_notes.py

---
+++ python/sqlite/sqlite3_notes.py
@@ -0,0 +1,23 @@
+import sqlite3
+conn = sqlite3.connect('example.db')
+c = conn.cursor()         # cursor object 
+
+# Create table
+c.execute('''CREATE TABLE IF NOT EXISTS stocks
+             (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+
+# Save (commit) the changes
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
+
+# Query 
+t = ('RHAT',)
+c.execute('SELECT * FROM stocks WHERE symbol=?', t) # avoiding sql injections by this syntax 
+c.fetchone()
+c.fetchall()
