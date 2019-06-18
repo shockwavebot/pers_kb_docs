7061a501557ab1f3aa913a44804bde8d837d8fec
mstan <stanojlovic.marko@gmail.com>
2019-06-18 14:42:44 +0200

    Update requests_notes.py

--- python/requests/requests_notes.py
+++ python/requests/requests_notes.py
@@ -1,3 +1,8 @@
+import requests
+
 # to get response in seconds 
 URL = 'http://localhost/app/service/validationservice'
 requests.post(URL, json={'dummy':'dummy'}).elapsed.total_seconds()
+
+# send post request 
+rsponse = requests.post(URL, data=JSON_DATA)
