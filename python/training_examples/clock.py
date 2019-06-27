4f8a8988c94ac1a5e322e8fb10e7d85af5cb927e
mstan <stanojlovic.marko@gmail.com>
2019-06-27 14:13:24 +0200

    Update clock.py

--- python/training_examples/clock.py
+++ python/training_examples/clock.py
@@ -1,26 +1,3 @@
-# v1
-class Clock(object):
-    def __init__(self, hour, minute):
-        self.hour = divmod(hour + divmod(minute,60)[0], 24)[1]
-        self.minute = divmod(minute, 60)[1]
-
-    @property
-    def time(self):
-        return f"{self.hour:02}:{self.minute:02}"
-
-    def __repr__(self):
-        return self.time
-
-    def __eq__(self, other):
-        return self.time == other
-
-    def __add__(self, minutes):
-        return Clock(self.hour + divmod(minutes,60)[0], self.minute + divmod(minutes,60)[1])
-
-    def __sub__(self, minutes):
-        return Clock(self.hour - divmod(minutes,60)[0], self.minute - divmod(minutes, 60)[1])
-
-# v2
 class Clock:
     def __init__(self, hour, minute):
         self.time = divmod((hour*60 + minute)%(24*60), 60)
