9c7c354c1074fd2f1e8f0a1fa6db1298f0c7dbe8
mstan <stanojlovic.marko@gmail.com>
2022-06-02 08:45:26 +0200

    Create key_mapping.sh

---
+++ linux/ubuntu/key_mapping.sh
@@ -0,0 +1,14 @@
+# Remap keys on the keyboard 
+
+# xev # to get a current key mapping 
+
+example="
+KeyRelease event, serial 37, synthetic NO, window 0x4400001,
+    root 0x163, subw 0x0, time 147968755, (988,315), root:(1038,429),
+    state 0x0, keycode 22 (keysym 0xff08, BackSpace), same_screen YES,
+    XLookupString gives 1 bytes: (08)
+    XFilterEvent returns: False "
+
+# To replace BackSpace key with Delete key 
+xmodmap -e "keycode 22 = Delete"
+
