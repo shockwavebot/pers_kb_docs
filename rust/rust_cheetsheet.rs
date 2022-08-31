ac324b460b3674cd9429784d415404ee8babe1e5
mstan <stanojlovic.marko@gmail.com>
2022-08-31 11:22:47 +0200

    Update rust_cheetsheet.rs

--- rust/rust_cheetsheet.rs
+++ rust/rust_cheetsheet.rs
@@ -1,7 +1,29 @@
 // cheats.rs 
 
+// ### Formating/Printing ###
+// macros defined in std::fmt
 dbg!(some_var);                 // showing in stderr
 println!("{:?}", some_var);     // showing debug in stdout 
+format!(""); // write formatted text to String type 
+// {:#?} pretty printing 
+// {:b} binary 
+// {:o} octal 
+// {:x} hexadecimal 
+// {:0>5} pad number with extra zeros (5 zeros in this example)
+
+// Any Struct can derive Debug, but Display must be explicitly implemented 
+#[derive(Debug)]
+
+use std::fmt;
+struct Structure(i32);
+impl fmt::Display for Structure {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+  write!(f, "{}", self.0)
+  }
+}
+
+
+// ### End of Formating/Printing ###
 
 // Casting - explicit type conversion with 'as'
 let my_integer = 1.12_f32 as u8;
@@ -9,6 +31,6 @@ let my_integer = 1.12_f32 as u8;
 // byte size of variable 
 std::mem::size_of_val(&x);
 
-// ## Vectors = growable array
-let mut vec: Vec<T> = Vec::new();   # vector of type T, you can't mix different types, and has to be defined, only sometimes infered 
-vec.push(elem);     # to add element to the vector 
+// ## Vectors = growable array ##
+let mut vec: Vec<T> = Vec::new();   // vector of type T, you can't mix different types, and has to be defined, only sometimes infered 
+vec.push(elem);     // to add element to the vector 
